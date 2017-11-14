e5bfe80936f02913f0e96145ba85ecfacba36792
syd53 <291676126@qq.com>
2017-11-14 10:35:58 +0800

    Add files via upload

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+from game_stats  import GameStats
+from button import Button
+from scoreboard import Scoreboard
+def run_game():
+    pygame.init()
+    #创建一个外星人 
+    ai_settings=Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("外星人大战")
+    #创建play按钮
+    play_button=Button(ai_settings,screen,"play")    
+    ship=Ship(ai_settings,screen)
+    #创建一个用于存储子弹的编组
+    bullets=Group()
+    aliens=Group()
+    #创建外星人群
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+    #创建存储游戏统计信息的实例，并创建记分牌
+    stats=GameStats(ai_settings)
+    sb=Scoreboard(ai_settings,screen,stats)
+# 开始游戏的主循环
+    while True:
+# 监视键盘和鼠标事件
+        gf.check_events(ai_settings,screen,stats,sb,play_button,ship,aliens,bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,stats,sb,ship,aliens,bullets)  
+            gf.update_aliens(ai_settings,screen,stats,sb,ship,aliens,bullets)
+        gf.update_screen(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button)
+run_game()
